27d1c07f875e648efd00117d0ff8b9e197844f73
William Sodré <71849007+william0035@users.noreply.github.com>
2021-06-06 17:39:41 -0300

    crud-catalogo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
+import AddTutorial from "./components/AddCatalog";
+import Tutorial from "./components/Catalog";
+import TutorialsList from "./components/CatalogList";
 
-
-const App = () => {
+function App() {
   return (
-    <>
-      App component
-    </>
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/tutorials" className="navbar-brand">
+          Exemplo Bootstrap
+        </a>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/tutorials"} className="nav-link">
+            Catalog
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/add"} className="nav-link">
+              Add Catalog
+            </Link>
+          </li>
+        </div>
+      </nav>
+
+      <div className="container mt-3">
+        <Switch>
+          <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
+          <Route exact path="/add" component={AddTutorial} />
+          <Route path="/tutorials/:id" component={Tutorial} />
+        </Switch>
+      </div>
+    </div>
   );
 }
 
